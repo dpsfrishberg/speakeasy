b0413af76869cce24a51ffa3217315d68df314d0
Daniel Frishberg <dfrishberg@frishberg.(none)>
2013-11-16 18:02:43 +0000

    Fixed setup.py package_data

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     url="http://www.speakeasycomments.com",
     packages=["speakeasy_core"],
     install_requires=["python-openid", "django_openid_auth", "threadedcomments"],
-    package_data={"templates": ["speakeasy_core/templates/*.html", "speakeasy_core/templates/*/*.html"]}
+    package_data={"speakeasy_core": ["templates/*.html", "templates/*/*.html"]}
 )
