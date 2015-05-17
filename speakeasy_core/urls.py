ede0d5f77b83522b980a8270279e0b1b3d52abd1
Daniel Frishberg <danielpfrishberg@gmail.com>
2015-05-17 17:51:05 +0000

    urls changes

--- speakeasy_core/urls.py
+++ speakeasy_core/urls.py
@@ -13,9 +13,7 @@ urlpatterns = patterns('',
     url(r'^article/(?P<slug>\w+)/post-comment.json$', views.post_comment, name="post_comment"),
     url(r'^article/(?P<slug>\w+)/check-comments.json$', views.check_comments),
     url(r'^article/(?P<slug>\w+)/(?P<timestamp>\d+)/get-node-changes.json$', views.get_node_changes),
-    url(r'^login/$', views.login_begin, name='openid-login'),
-    url(r'^login-complete/$', 'django_openid_auth.views.login_complete', name='openid-complete'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/',}, name='logout'),
+    #url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/',}, name='logout'),
     
     url(r'^account/create/$', views.create_account),
     url(r'^account/list/$', views.list_domains)
